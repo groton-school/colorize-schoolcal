1b6c045e406593aaa68649365dc0b90e9f8e14b7
Seth Battis <sbattis@groton.org>
2023-03-03 02:21:54 +0000

    colorize arbitrary search string

--- src/Calendar.ts
+++ src/Calendar.ts
@@ -4,32 +4,74 @@ const blocks = {
     RD: '11',
     OR: '6',
     YL: '5',
-    GR: '10',
-    LB: '1',
+    GR: '2',
+    LB: '7',
     DB: '9',
     PR: '3',
 };
 
+function enumKeys<O extends object, K extends keyof O = keyof O>(obj: O): K[] {
+    return Object.keys(obj).filter((k) => Number.isNaN(+k)) as K[];
+}
+
 export function onHomepage() {
     return g.CardService.Card.create({
         name: 'Colorize SchoolCal',
-        widgets: Object.keys(blocks).map((code) =>
-            g.CardService.Widget.newTextButton({
-                text: `Colorize ${code}`,
-                functionName: 'colorize',
-                parameters: { code },
-            })
-        ),
+        sections: [
+            g.CardService.Card.newCardSection({
+                header: 'Set block Cclors',
+                widgets: Object.keys(blocks).map((code) =>
+                    g.CardService.Widget.newTextButton({
+                        text: `Colorize ${code}`,
+                        functionName: 'colorize',
+                        parameters: { code },
+                    })
+                ),
+            }),
+            g.CardService.Card.newCardSection({
+                header: 'Set arbitrary colors',
+                widgets: [
+                    'Set the color of all synced events based on a search query. Caution: too broad a query will time out and fail!',
+                    CardService.newTextInput()
+                        .setTitle('search query')
+                        .setFieldName('query'),
+                    CardService.newSelectionInput()
+                        .setTitle('color')
+                        .setFieldName('color')
+                        .setType(CardService.SelectionInputType.DROPDOWN)
+                        .addItem('Pale Blue', '1', false)
+                        .addItem('Pale Green', '2', false)
+                        .addItem('Mauve', '3', false)
+                        .addItem('Pale Red', '4', false)
+                        .addItem('Yellow', '5', false)
+                        .addItem('Orange', '6', false)
+                        .addItem('Cyan', '7', false)
+                        .addItem('Gray', '8', false)
+                        .addItem('Blue', '9', false)
+                        .addItem('Green', '10', false)
+                        .addItem('Red', '11', false),
+                    g.CardService.Widget.newTextButton({
+                        text: 'Colorize',
+                        functionName: 'colorize',
+                    }),
+                ],
+            }),
+        ],
     });
 }
 
-global.colorize = ({ parameters: { code } }) => {
+global.colorize = ({
+    parameters: { code = null },
+    formInput: { query = null, color = null },
+}) => {
+    color = query && color;
     const user = Session.getActiveUser();
     const calendar = CalendarApp.getCalendarsByName(user.getEmail())[0];
     const events = calendar.getEvents(
         new Date('2023-01-01'),
         new Date('2023-06-30'),
-        { author: 'schoolcal@groton.org', search: `- ${code}` }
+        { author: 'schoolcal@groton.org', search: query || `- ${code}` }
     );
-    events.forEach((event) => event.setColor(blocks[code]));
+    events.forEach((event) => event.setColor(color || blocks[code]));
+    return g.CardService.Navigation.replaceStack(onHomepage());
 };
