767679803f9fdd9bf93f0c2318e9ef698712659b
Seth Battis <sbattis@groton.org>
2023-07-08 10:11:31 -0400

    update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,3 @@
-const config = require('@battis/gas-lighter/webpack.config');
-
-module.exports = config({ root: __dirname });
+module.exports = require('@battis/gas-lighter/webpack.config')({
+  root: __dirname
+});
